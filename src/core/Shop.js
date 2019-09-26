9190554cfba11b264f7a4ce959185e277dd0d0cf
Filipe Soares <filipecarlossoares@gmail.com>
2019-09-26 20:36:06 -0300

    Handling with filter by category

--- src/core/Shop.js
+++ src/core/Shop.js
@@ -5,6 +5,9 @@ import { getCategories } from "./apiCore"
 import Checkbox from "./Checkbox"
 
 const Shop = () => {
+    const [myFilters, setMyFilters] = useState({
+        filters: { category: [], price: [] }
+    });
     const [categories, setCategories] = useState([])
     const [error, setError] = useState(false)
 
@@ -22,6 +25,12 @@ const Shop = () => {
         init()
     }, [])
 
+    const handleFilters = (filters, filterBy) => {
+        const newFilters = { ...myFilters }
+        newFilters.filters[filterBy] = filters
+        setMyFilters(newFilters)
+    }
+
     return (
         <Layout
             title="Shop Page"
@@ -32,11 +41,12 @@ const Shop = () => {
                 <div className="col-4">
                 <h4>Filter by categories</h4>
                     <ul>
-                        <Checkbox categories={categories} />
+                        <Checkbox categories={categories} handleFilters={filters =>
+                                handleFilters(filters, "category")} />
                     </ul>
                 </div>
 
-                <div className="col-8">right</div>
+                <div className="col-8">{JSON.stringify(myFilters)}</div>
             </div>
         </Layout>
     );
