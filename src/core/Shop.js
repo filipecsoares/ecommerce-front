bb76417c73531101c2da7dd8dd80826ce513cdd5
Filipe Soares <filipecarlossoares@gmail.com>
2019-09-27 20:19:21 -0300

    Filter by price range

--- src/core/Shop.js
+++ src/core/Shop.js
@@ -1,8 +1,10 @@
-import React, { useState, useEffect } from "react"
-import Layout from "./Layout"
-import Card from "./Card"
-import { getCategories } from "./apiCore"
-import Checkbox from "./Checkbox"
+import React, { useState, useEffect } from 'react'
+import Layout from './Layout'
+import Card from './Card'
+import { getCategories } from './apiCore'
+import Checkbox from './Checkbox'
+import RadioBox from './RadioBox'
+import { prices } from './fixedPrices'
 
 const Shop = () => {
     const [myFilters, setMyFilters] = useState({
@@ -28,7 +30,23 @@ const Shop = () => {
     const handleFilters = (filters, filterBy) => {
         const newFilters = { ...myFilters }
         newFilters.filters[filterBy] = filters
+        if (filterBy === "price") {
+            let priceValues = handlePrice(filters)
+            newFilters.filters[filterBy] = priceValues
+        }
         setMyFilters(newFilters)
+    }
+
+    const handlePrice = value => {
+        const data = prices
+        let array = []
+
+        for (let key in data) {
+            if (data[key]._id === parseInt(value)) {
+                array = data[key].array
+            }
+        }
+        return array
     }
 
     return (
@@ -39,11 +57,20 @@ const Shop = () => {
         >
             <div className="row">
                 <div className="col-4">
-                <h4>Filter by categories</h4>
+                    <h4>Filter by categories</h4>
                     <ul>
                         <Checkbox categories={categories} handleFilters={filters =>
                                 handleFilters(filters, "category")} />
                     </ul>
+                    <h4>Filter by price range</h4>
+                    <div>
+                        <RadioBox
+                            prices={prices}
+                            handleFilters={filters =>
+                                handleFilters(filters, "price")
+                            }
+                        />
+                    </div>
                 </div>
 
                 <div className="col-8">{JSON.stringify(myFilters)}</div>
