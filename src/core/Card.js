ded973e673b5ccb0376087216f6bcc3f39934e29
Filipe Soares <filipecarlossoares@gmail.com>
2019-10-09 20:07:49 -0300

    Remove product from cart

--- src/core/Card.js
+++ src/core/Card.js
@@ -2,13 +2,15 @@ import React, { useState } from "react";
 import { Link, Redirect } from "react-router-dom";
 import ShowImage from "./ShowImage";
 import moment from "moment";
-import { addItem, updateItem } from "./cartHelpers";
+import { addItem, updateItem, removeItem } from "./cartHelpers";
 
-const Card = ({ 
+const Card = ({
     product,
     showViewProductButton = true,
     showAddToCartButton = true,
-    cartUpdate = false }) => {
+    cartUpdate = false,
+    showRemoveProductButton = false
+}) => {
     const [redirect, setRedirect] = useState(false);
     const [count, setCount] = useState(product.count);
 
@@ -48,7 +50,20 @@ const Card = ({
             )
         );
     };
-    
+
+    const showRemoveButton = showRemoveProductButton => {
+        return (
+            showRemoveProductButton && (
+                <button
+                    onClick={() => removeItem(product._id)}
+                    className="btn btn-outline-danger mt-2 mb-2"
+                >
+                    Remove Product
+                </button>
+            )
+        );
+    };
+
     const showStock = quantity => {
         return quantity > 0 ? (
             <span className="badge badge-primary badge-pill">In Stock</span>
@@ -109,6 +124,8 @@ const Card = ({
                 {showViewButton(showViewProductButton)}
 
                 {showAddToCart(showAddToCartButton)}
+
+                {showRemoveButton(showRemoveProductButton)}
 
                 {showCartUpdateOptions(cartUpdate)}
             </div>
