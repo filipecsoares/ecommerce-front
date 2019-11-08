0a29012df4a8d3b8d63114631392b1c481d4a8cd
Filipe Soares <filipecarlossoares@gmail.com>
2019-11-07 21:30:30 -0300

    Update Product

--- src/Routes.js
+++ src/Routes.js
@@ -15,6 +15,7 @@ import Cart from "./core/Cart";
 import Orders from "./admin/Orders";
 import Profile from "./user/Profile";
 import ManageProducts from "./admin/ManageProducts";
+import UpdateProduct from "./admin/UpdateProduct";
 
 const Routes = () => {
     return (
@@ -56,6 +57,11 @@ const Routes = () => {
                     path="/admin/products"
                     exact
                     component={ManageProducts}
+                />
+                <AdminRoute
+                    path="/admin/product/update/:productId"
+                    exact
+                    component={UpdateProduct}
                 />
             </Switch>
         </BrowserRouter>
